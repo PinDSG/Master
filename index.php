67ebaab5df3c8f374170e4e049c4a7f5770910aa
PinDSG <56348700+PinDSG@users.noreply.github.com>
2019-10-09 18:50:40 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+    echo "Nama          :   Davind Anugraha Putra|Biasa dipanggil Dapind,PinDSG";
+  	echo "<br>";
+  	echo "Kelas         :   X RPL 1";
+  	echo "<br>";
+    echo "Nomor Absen   :   11";
+?>
